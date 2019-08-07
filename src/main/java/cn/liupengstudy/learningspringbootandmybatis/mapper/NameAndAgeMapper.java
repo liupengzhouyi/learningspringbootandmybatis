288d4c15385eee74d128bcc9ffdeda2f0f43c2de
liupeng <LIUPENG.0@outlook.com>
2019-08-08 00:44:41 +0800

    了解generatorConfiguration V 1.0.4

--- src/main/java/cn/liupengstudy/learningspringbootandmybatis/mapper/NameAndAgeMapper.java
+++ src/main/java/cn/liupengstudy/learningspringbootandmybatis/mapper/NameAndAgeMapper.java
@@ -3,6 +3,7 @@ package cn.liupengstudy.learningspringbootandmybatis.mapper;
 import cn.liupengstudy.learningspringbootandmybatis.pojo.NameAndAge;
 
 public interface NameAndAgeMapper {
+
     int insert(NameAndAge record);
 
     int insertSelective(NameAndAge record);
