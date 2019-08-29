7a5ffe72e67a3e2f382526801d72bd0f54e11919
liupeng <LIUPENG.0@outlook.com>
2019-08-29 15:31:16 +0800

    另一种接收Json参数 V 1.0.14

--- src/main/java/cn/liupengstudy/learningspringbootandmybatis/controller/GetJsinController.java
+++ src/main/java/cn/liupengstudy/learningspringbootandmybatis/controller/GetJsinController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/getJsonB")
 public class GetJsinController {
 
+    // 直接通过@RequestBody 的方式，直接将json的数据注入到了JSONObject里面了。
+    // param jsonobject 这个是阿里的 fastjson对象
     @RequestMapping(value = "/active", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public String active(@RequestBody ActiveRequest request) {
         System.out.println(request.toString());
