4516290657425a578eb2aff52b7edea6c4886277
liupeng <LIUPENG.0@outlook.com>
2019-09-04 23:20:57 +0800

    SQL.员工登录表 V 1.0.23

--- src/main/resources/static/databaseSQL.sql
+++ src/main/resources/static/databaseSQL.sql
@@ -15,5 +15,18 @@ create table employeesTable
 		primary key (employeesID)
 );
 
+# 员工登录表
+create table employeesLoginTable
+(
+    employeesLoginID int(10) auto_increment comment '员工登录ID',
+    employeesID varchar(30) null comment '员工ID',
+    loginDateTime varchar(40) null comment '登录时间',
+    IPAddress varchar(20) null comment '登录IP地址',
+    longitude double null comment '经度',
+    latitude double null comment '纬度',
+    loginResults int null comment '登录结果',
+    constraint employeesLoginTable_pk
+        primary key (employeesLoginID)
+)
+    comment '员工登录表';
 
-#
